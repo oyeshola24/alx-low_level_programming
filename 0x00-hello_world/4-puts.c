67cda76c3f2bf61e2082d87e40db724f634105ab
oyeshola24 <ohrms.henry@gmail.com>
2022-06-02 18:26:14 -0700

    ‘quest04’

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+/**
+   * main - print to screen
+   *
+   * Return: zero
+   */
+int main(void)
+{
+	char Gola[] = "\"Programming is like building a multilingual puzzle";
+
+	puts(Gola);
+	return (0);
+}
