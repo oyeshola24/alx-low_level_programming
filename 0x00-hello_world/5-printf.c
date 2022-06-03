ecd56602c95f7f84a39a36afc70bdb335611bf0b
oyeshola24 <ohrms.henry@gmail.com>
2022-06-02 20:03:23 -0700

    ‘five’

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,11 +1,11 @@
 #include <stdio.h>
 /**
-   * main - Prints command on to the screen
-   *
-   * Return the value zero
-   */
+ * main - Starts Here
+ *
+ * Return: Always  be 0 which means Success
+ */
 int main(void)
 {
-	printf("with proper grammar, but the outcome is a piece of art,\n");
-	return (0);
+printf("with proper grammar, but the outcome is a piece of art,\n");
+return (0);
 }
