032f73c6cfb6699a19a5c1cc6a5e76405d851fb2
oyeshola24 <ohrms.henry@gmail.com>
2022-06-02 20:22:53 -0700

    ‘six’

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,19 +1,19 @@
 #include <stdio.h>
 /**
- *main - Prints the sizeof char,int, float,long int and long long int
- *Return: 0 if Success
- */
+   * main - data types
+   *
+   *Return: 0
+   */
 int main(void)
 {
-char car;
-int integer;
-long int lngint;
-long long int lnglng;
-float flow;
-printf("Size of a char: %d byte(s)\n", sizeof(car));
-printf("Size of an int: %d byte(s)\n", sizeof(integer));
-printf("Size of a float: %d byte(s)\n", sizeof(flow));
-printf("Size of a long int: %d byte(s)\n", sizeof(lngint));
-printf("Size of a long long int: %d byte(s)\n", sizeof(lnglng));
-return (0);
+	char c;
+	int i;
+	float f;
+
+	printf("Size of a char: %lu byte\(s\)\n", sizeof(c));
+	printf("Size of an int: %lu byte\(s\)\n", sizeof(i));
+	printf("Size of a long int: %lu byte\(s\)\n", sizeof(long int));
+	printf("Size of a long long int: %lu byte\(s\)\n", sizeof(long long int));
+	printf("Size of a float: %lu byte\(s\)\n", sizeof(f));
+	return (0);
 }
