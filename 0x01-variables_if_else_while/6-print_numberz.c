e01fd86d9038d108522564bfc28b96d31900c8df
oyeshola24 <ohrms.henry@gmail.com>
2022-06-03 21:37:04 -0700

    quest9

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * main - 0 to 9
+ *
+ * Return: 0
+ */
+int main(void)
+{
+int i;
+for (i = 48; i < 58; i++)
+{
+putchar(i);
+}
+putchar('\n');
+return (0);
+}
---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * main -  lowercase backwards
+ *
+ * Return: 0
+ */
+int main(void)
+{
+int i;
+for (i = 122; i > 96; i--)
+{
+putchar(i);
+}
+putchar('\n');
+return (0);
+}
