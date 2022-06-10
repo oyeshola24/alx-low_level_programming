c344257f95511f3b9a1504b2c474fe22b7523b4e
oyeshola24 <ohrms.henry@gmail.com>
2022-06-09 20:32:30 -0700

    quest5

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+* more_numbers - This is to  print more numbers
+*/
+void more_numbers(void)
+{
+	int a, b;
+
+	for (a = 0; a < 10; a++)
+	{
+		for (b = 0; b < 15; b++)
+		{
+			if (b >= 10)
+				_putchar((b / 10) + '0');
+			_putchar((b % 10) + '0');
+		}
+		_putchar('\n');
+	}
+}
