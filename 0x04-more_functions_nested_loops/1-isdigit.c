0f9a41525b63770fdc76d0736751d0382da63511
oyeshola24 <ohrms.henry@gmail.com>
2022-06-09 20:25:35 -0700

    quest2

---
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,11 @@
+#include "main.h"
+
+/**
+* _isdigit - This funtion checks for a digit if available
+* @c: an argument of variable int
+* Return: digit
+*/
+int _isdigit(int c)
+{
+	return (c >= '0' && c <= '9');
+}
---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+* mul - multipication of two integers
+* @a: First input integer
+* @d: Second input integer
+* Return: value of the multipication
+*/
+
+int mul(int a, int d)
+{
+	return (a * d);
+}
