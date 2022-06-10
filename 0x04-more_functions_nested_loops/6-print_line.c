e41463ec7998313b5083624c1c8c620c504a0902
oyeshola24 <ohrms.henry@gmail.com>
2022-06-09 20:45:29 -0700

    qu6

--- 0x04-more_functions_nested_loops/6-print_line.c
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -1,8 +1,8 @@
 #include "main.h"
 
 /**
-* void print_line -> This is to print a line
-* @n: an argument with variable type int
+* print_line -> printing line
+* @n: integer params
 */
 void print_line(int n)
 {
@@ -13,7 +13,7 @@ void print_line(int n)
 	else
 	{
 		for (x = 0; x < n; x++)
-			_putchar(85);
+			_putchar(95);
 		_putchar('\n');
 	}
 }
