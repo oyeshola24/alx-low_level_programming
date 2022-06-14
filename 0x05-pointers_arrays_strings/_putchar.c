0449574cdada387ff92fb9735951da75b3b9fe38
oyeshola24 <ohrms.henry@gmail.com>
2022-06-13 19:05:09 -0700

    done

--- 0x05-pointers_arrays_strings/_putchar.c
+++ 0x05-pointers_arrays_strings/_putchar.c
@@ -2,12 +2,12 @@
 
 /**
  * _putchar - writes the character c to stdout
- * @b: The character to print
+ * @c: The character to print
  * 
  * Return: On success 1
  * On error, -1 is returned, and error is configured appropriately
  */
-int _putchar(char b)
+int _putchar(char c)
 {
-    return (write(1, &b, 1));
+    return (write(1, &c, 1));
 }
--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -1,6 +1,6 @@
-#ifndef MAIN.H
-#define MAIN.H
-int _putchar(char b);
+#ifndef MAIN_H
+#define MAIN_H
+int _putchar(char c);
 void reset_to_98(int *n);
 
 #endif
